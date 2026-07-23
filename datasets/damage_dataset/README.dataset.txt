d5e53236319703e5cd51b311cbca290cd115d18a
Mohamed Vaseem <vaseemmansoor2007@gmail.com>
2026-07-23 23:00:39 +0530

    YOLO

---
+++ datasets/damage_dataset/README.dataset.txt
@@ -0,0 +1,6 @@
+# car damage segmentation > 2023-08-14 2:06pm
+https://universe.roboflow.com/agni-4nqn3/car-damage-segmentation-bk7wi
+
+Provided by a Roboflow user
+License: CC BY 4.0
+
---
+++ train_damage.py
@@ -0,0 +1,32 @@
+from ultralytics import YOLO
+
+
+def main():
+
+    model = YOLO("yolo11n-seg.pt")
+
+    model.train(
+        data="datasets/damage_dataset/data.yaml",
+
+        epochs=50,
+        imgsz=640,
+
+        batch=8,
+        workers=4,
+
+        device=0,
+
+        cache=True,
+        pretrained=True,
+
+        project="training",
+        name="damage_segmentation",
+
+        patience=20,
+        save=True,
+        verbose=True
+    )
+
+
+if __name__ == "__main__":
+    main()
